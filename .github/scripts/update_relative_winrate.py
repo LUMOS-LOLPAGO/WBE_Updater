c1160f50146ee478f0bd71c6adc82e2da4bb598e
bong-u <bongudev@gmail.com>
2025-07-25 18:46:07 +0900

    feat: 상대 승률 주기적 갱신 gh-action 구현

---
+++ .github/scripts/update_relative_winrate.py
@@ -0,0 +1,46 @@
+import sys
+import requests
+import json
+from common import configure_logger, load_env, ServerRequestError
+
+logger = configure_logger()
+
+# 환경 변수 불러오기
+env = load_env(["SERVER_URL"])
+SERVER_URL = env.get("SERVER_URL")
+
+TIER_LIST = [
+    "IRON",
+    "BRONZE",
+    "SILVER",
+    "GOLD",
+    "PLATINUM",
+    "DIAMOND",
+    "MASTER",
+    "GRANDMASTER",
+    "CHALLENGER",
+]
+
+
+def update_relative_winrate(tier: str) -> str:
+    url = f"{SERVER_URL}/relative-winrate/{tier}"
+    res = requests.put(url)
+    if res.status_code != 200:
+        raise ServerRequestError(
+            f"{tier} 티어 상대승률 업데이트 실패: {res.status_code} - {json.loads(res.text)}"
+        )
+    return str(json.loads(res.text))
+
+
+if __name__ == "__main__":
+    failed_tiers = []
+    for tier in TIER_LIST:
+        try:
+            result_message = update_relative_winrate(tier)
+            logger.info(result_message)
+        except ServerRequestError as e:
+            logger.error(e)
+            failed_tiers.append(tier)
+
+    if failed_tiers:
+        sys.exit(1)
